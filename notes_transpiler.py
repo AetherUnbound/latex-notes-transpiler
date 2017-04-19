5f5dc46559b9002a5254b45ea74a0e3ebc418ec1
Aetherunbound <bowdenm@spu.edu>
2017-04-18 20:33:40 -0700

    Initial commit with basic transpiling functionality

---
+++ notes_transpiler.py
@@ -0,0 +1,73 @@
+import os
+import sys
+
+
+def create_tex(template_file, input_file, output_file):
+    with open(template_file, 'r') as template:
+        template_lines = template.readlines()
+        template_lines = [line.strip('\n') for line in template_lines]
+        start_index = template_lines.index("%%%TEXT GOES HERE%%%")
+        tex_lines = transpile(input_file)
+        out_lines = template_lines[:start_index] + tex_lines + template_lines[start_index + 1:]
+        # print("\n".join(out_lines))
+        with open(output_file, 'w') as out:
+            out.write('\n'.join(out_lines))
+
+
+def transpile(filename):
+    with open(filename, 'r') as f:
+        lines = list(map(lambda x: x.replace("    ", "\t").strip('\n'), f.readlines()))
+        return tex_body(lines)
+
+def tex_body(input):
+    level = 0
+    i = 0
+    output = []
+    while True:
+        if i == len(input):
+            break
+        if level == 0:
+            output.append("\\subsection*{" + input[i] + "}")
+            i += 1
+            if i == len(input):
+                break
+        curr_level = len(input[i]) - len(input[i].lstrip('\t'))
+        if curr_level > 0:
+            out, i, level = to_itemize(input, i, curr_level)
+            output.extend(out)
+        else: break
+    return output
+
+
+def to_itemize(input, loc, level):
+    output = []
+    i = loc
+    while True:
+        output.append("\t" * level + "\\begin{itemize}")
+        while True:
+            s_out = input[i].lstrip('\t')
+            s_out = "\\item " + s_out[0].upper() + s_out[1:]
+            output.append("\t" * (level + 1) + s_out)
+            i += 1
+            if i == len(input):
+                level = 0
+                break
+            next_level = len(input[i]) - len(input[i].lstrip('\t'))
+            # go deeper
+            if (next_level > level):
+                out, i, level = to_itemize(input, i, next_level)
+                output.extend(out)
+
+            elif(next_level < level):
+                output.append("\t" * level + "\\end{itemize}")
+                level = next_level
+                return output, i, level
+            if (level == 0): break
+        if (level == 0):
+            output.append("\t" * (level + 1) + "\\end{itemize}")
+            break
+    return output, i, level
+
+
+if __name__ == "__main__":
+    create_tex("template.tex", "example_notes_small.txt", "output.txt")
